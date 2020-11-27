9ec213aeb12d8ba524fbac8e776675cb79a3750a
Trung Luu <luuductrung1234@gmail.com>
2020-11-27 23:07:02 +0700

    init first app

---
+++ myapp/src/main/java/com/learn/App.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ApplicationContext context;
+    }
+}
