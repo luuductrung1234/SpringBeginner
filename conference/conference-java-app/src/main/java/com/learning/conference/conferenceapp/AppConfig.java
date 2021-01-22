929f7c64db1e24d6ece17bf9ae434eda40760ac8
Trung Luu <luuductrung1234@gmail.com>
2021-01-22 20:27:10 +0700

    FactoryBean & SpEL

--- conference/conference-java-app/src/main/java/com/learning/conference/conferenceapp/AppConfig.java
+++ conference/conference-java-app/src/main/java/com/learning/conference/conferenceapp/AppConfig.java
@@ -2,15 +2,30 @@ package com.learning.conference.conferenceapp;
 
 import com.learning.conference.conferenceapp.repository.*;
 import com.learning.conference.conferenceapp.services.*;
+import com.learning.conference.conferenceapp.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.learning"})
 public class AppConfig {
+
+    @Bean("calendarFactory")
+    public CalendarFactory calendarFactory() {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar calendar() throws Exception {
+        return calendarFactory().getObject();
+    }
 
     // @Bean annotation is only at method level
     @Bean(name = "speakerService")
--- conference/conference-java-app/src/main/java/com/learning/conference/conferenceapp/model/Speaker.java
+++ conference/conference-java-app/src/main/java/com/learning/conference/conferenceapp/model/Speaker.java
@@ -3,13 +3,15 @@ package com.learning.conference.conferenceapp.model;
 public class Speaker {
     private String firstName;
     private String lastName;
+    private double seedNum;
 
     public Speaker() {
     }
 
-    public Speaker(String firstName, String lastName) {
+    public Speaker(String firstName, String lastName, double seedNum) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.seedNum = seedNum;
     }
 
     public String getFirstName() {
@@ -26,5 +28,13 @@ public class Speaker {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public double getSeedNum() {
+        return seedNum;
+    }
+
+    public void setSeedNum(double seedNum) {
+        this.seedNum = seedNum;
     }
 }
