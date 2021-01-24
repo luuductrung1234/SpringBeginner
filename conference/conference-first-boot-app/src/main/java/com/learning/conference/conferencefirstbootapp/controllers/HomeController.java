52e9eb15bc3bbe1d124e45e4bb5cd4bce14bdf4c
Trung Luu <luuductrung1234@gmail.com>
2021-01-24 13:44:20 +0700

    replace embedded container Tomcat with Jetty

--- conference/conference-first-boot-app/src/main/java/com/learning/conference/conferencefirstbootapp/controllers/HomeController.java
+++ conference/conference-first-boot-app/src/main/java/com/learning/conference/conferencefirstbootapp/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.learning.conference.conferencefirstbootapp.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +15,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/home")
 public class HomeController {
+    private Logger logger = LogManager.getLogger();
+
     @Value("${app.version}")
     private String appVersion;
 
     @Value("${spring.profiles.active}")
     private String activeProfile;
+
+    @Value("${server.port}")
+    private Integer serverPort;
 
     @GetMapping
     @RequestMapping("/status")
@@ -24,6 +32,7 @@ public class HomeController {
         return ResponseEntity.ok(new HashMap<String, String>(){{
             put("app-version", appVersion);
             put("active-profile", activeProfile);
+            put("server-port", serverPort.toString());
         }});
     }
 }
