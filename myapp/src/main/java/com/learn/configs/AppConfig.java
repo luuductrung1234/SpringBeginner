7b0b6c0548c2467ad70223572aae38b34ca9919d
Trung Luu <luuductrung1234@gmail.com>
2021-02-12 17:47:16 +0700

    define BeanFactoryPostProcessor & custom Scope

--- myapp/src/main/java/com/learn/configs/AppConfig.java
+++ myapp/src/main/java/com/learn/configs/AppConfig.java
@@ -1,8 +1,11 @@
 package com.learn.configs;
 
 import com.learn.repositories.MyRepositoryImpl;
+import com.learn.seedworks.InspectRegisteredScopesPostProcessor;
 import com.learn.services.TheirService;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
 @ComponentScan(basePackages = "com.learn")
@@ -12,5 +15,17 @@ public class AppConfig {
     @Scope("prototype")
     public TheirService getTheirService() {
         return new TheirService(new MyRepositoryImpl());
+    }
+
+    @Bean
+    public static CustomScopeConfigurer getCustomScopeConfigurer(){
+        var customScopeConfigurer = new CustomScopeConfigurer();
+        customScopeConfigurer.addScope("thread", new SimpleThreadScope());
+        return customScopeConfigurer;
+    }
+
+    @Bean
+    public static InspectRegisteredScopesPostProcessor getInspectScopesPostProcessor(){
+        return new InspectRegisteredScopesPostProcessor();
     }
 }
---
+++ myapp/src/main/java/com/learn/seedworks/InspectRegisteredScopesPostProcessor.java
@@ -0,0 +1,30 @@
+package com.learn.seedworks;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+
+import java.util.Arrays;
+
+public class InspectRegisteredScopesPostProcessor implements BeanFactoryPostProcessor, Ordered {
+
+    private int order = Ordered.LOWEST_PRECEDENCE;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        Arrays.asList(beanFactory.getRegisteredScopeNames()).forEach(scopeName -> {
+            var scope = beanFactory.getRegisteredScope(scopeName);
+            System.out.println("Registered scope [name]:" + scopeName + " with [instance]:" + scope + ".\n");
+        });
+    }
+
+    public void setOrder(int order){
+        this.order = order;
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+}
