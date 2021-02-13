10c415a9a222e232a360d24b568e0f8de65c84e1
Trung Luu <luuductrung1234@gmail.com>
2021-02-13 12:38:51 +0700

    composing Java-based configurations

--- myapp/src/main/java/com/learn/configs/AppConfig.java
+++ myapp/src/main/java/com/learn/configs/AppConfig.java
@@ -1,15 +1,13 @@
 package com.learn.configs;
 
 import com.learn.repositories.MyRepositoryImpl;
-import com.learn.seedworks.InspectRegisteredScopesPostProcessor;
 import com.learn.services.TheirService;
-import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.*;
-import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
 @ComponentScan(basePackages = "com.learn")
 @PropertySource("classpath:application.properties")
+@Import(SpringContainerConfig.class)
 public class AppConfig {
     @Bean
     @Scope("prototype")
@@ -17,15 +15,4 @@ public class AppConfig {
         return new TheirService(new MyRepositoryImpl());
     }
 
-    @Bean
-    public static CustomScopeConfigurer getCustomScopeConfigurer(){
-        var customScopeConfigurer = new CustomScopeConfigurer();
-        customScopeConfigurer.addScope("thread", new SimpleThreadScope());
-        return customScopeConfigurer;
-    }
-
-    @Bean
-    public static InspectRegisteredScopesPostProcessor getInspectScopesPostProcessor(){
-        return new InspectRegisteredScopesPostProcessor();
-    }
 }
